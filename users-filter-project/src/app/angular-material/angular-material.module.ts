af8456f0802662e46a64e7c2f6651102200a3bc4
brunohsrosa <brunohsrosa@live.com>
2026-04-17 20:38:16 -0300

    Criação layout componente de detalhes, arquivo de estilos scss

--- users-filter-project/src/app/angular-material/angular-material.module.ts
+++ users-filter-project/src/app/angular-material/angular-material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports: [
-    MatButtonModule,
-    MatIconModule
+      MatListModule,
+      MatDividerModule
   ],
   exports: [
-        MatButtonModule,
-        MatIconModule
+      MatListModule,
+      MatDividerModule
   ],
 })
 
