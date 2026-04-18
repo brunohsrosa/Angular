3703ee28705fd9bbd2e4a1c4fd490aa9fbcd8e6f
brunohsrosa <brunohsrosa@live.com>
2026-04-18 07:37:36 -0300

    Binding, Dumb Components, Optional Chaining

--- users-filter-project/src/app/components/user-details/user-details.component.ts
+++ users-filter-project/src/app/components/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUser } from 'src/app/interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent {
-
+  @Input({required: true}) user: IUser = {} as IUser;
+  /* Inicializa a propriedade user como um objeto vazio do tipo IUser */
+  /* required: true indica que a propriedade user é obrigatória e deve ser fornecida pelo componente pai */
 }
